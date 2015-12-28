d3f1c6254a8ac627cf269e277f3992ed2fd08afa
Peter Hsu <sysu.peter.hsu@gmail.com>
2015-12-28 14:29:02 +0800

    Create extend-class.js

---
+++ extend-class.js
@@ -0,0 +1,26 @@
+class Monster{
+  constructor(name, health){
+    
+  }
+}
+
+class ExtendedMonster extends Monster{
+  constructor(){
+    super(...)
+  }
+}
+
+class ExtendedMonster2 extends getClass() {
+
+}
+
+function getClass(){
+  if(isIE()){
+    return IEWebSocketImpl;
+  }
+  return WebSocket
+  
+  function isIE(){
+    return false
+  }
+}
