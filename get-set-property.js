f0bbed7cb3eff2a3a23f7eb32f1c4814328478b6
Peter Hsu <sysu.peter.hsu@gmail.com>
2015-12-28 14:19:11 +0800

    Update get-set-property.js

--- get-set-property.js
+++ get-set-property.js
@@ -1,1 +1,19 @@
 
+class Monster{
+  constructor(name, health){
+    this.name = name
+    this.health = health
+  }
+  
+  get isAlive(){
+    return this.health > 0
+  }
+  
+  set isAlive(alive){
+    this.health = alive
+  }
+}
+
+var monster = new Monster("J.A.R.V.I.S", 100)
+console.log(monster.isAlive)
+monster.isAlive = 200
